d3e867b92cb24199966a01c1a041fefd84912a42
forootan <ali_teimoury1@yahoo.com>
2022-10-29 11:55:47 -0500

    fixed some bugs in server

--- server/server.go
+++ server/server.go
@@ -67,14 +67,14 @@ func main() {
 }
 
 func handleSocket(client_to_proxy net.Conn) {
-	buffer := make([]byte, 8*1024)
+	buffer := make([]byte, 9*1024)
 	length, e := client_to_proxy.Read(buffer)
 	if e != nil {
 		fmt.Println("ERROR1 ", e)
 		return
 	}
 	if e != nil {
-		fmt.Println("ERROR1 ", e)
+		fmt.Println("ERROR2 ", e)
 		return
 	}
 
@@ -85,13 +85,15 @@ func handleSocket(client_to_proxy net.Conn) {
 		break
 
 	case "Base64":
-		message = string(decodeBase64(buffer, length))
+		message = string(decodeBase64(buffer, len(buffer)))
 		break
 
 	case "AES":
-		message = string(decryptAES(buffer, length, jjServerConfig.ListenEncryptionKey))
+		message = string(decryptAES(buffer, len(buffer), jjServerConfig.ListenEncryptionKey))
 		break
 	}
+
+	fmt.Println(message)
 
 	if !strings.Contains(message, "\r\n") {
 		fmt.Println("Wrong UserPass")
@@ -102,6 +104,7 @@ func handleSocket(client_to_proxy net.Conn) {
 		splited := strings.Split(message, "\r\n")
 		splited = strings.Split(splited[0], ",")
 		if len(splited) > 1 {
+			message = message[strings.Index(message, "\r\n")+2:]
 			if splited[0] != jjServerConfig.UserName || splited[1] != jjServerConfig.Password {
 				fmt.Println("Wrong UserPass")
 				return
@@ -114,12 +117,12 @@ func handleSocket(client_to_proxy net.Conn) {
 		//message = strings.Replace(message, "CONNECT", "GET", 1)
 		proxy_to_server, e := net.Dial("tcp", splited[1])
 		if e != nil {
-			fmt.Println("ERROR2 ", e)
+			fmt.Println("ERROR3 ", e)
 			return
 		}
 		lenn, e := client_to_proxy.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 		if e != nil {
-			fmt.Println("ERROR8 ", e)
+			fmt.Println("ERROR4 ", e)
 			return
 		}
 		fmt.Println(lenn)
@@ -136,7 +139,7 @@ func handleSocket(client_to_proxy net.Conn) {
 		}
 		proxy_to_server, e := net.Dial("tcp", final_host+":80")
 		if e != nil {
-			fmt.Println("ERROR7 ", e)
+			fmt.Println("ERROR5 ", e)
 			return
 		}
 		_, e = proxy_to_server.Write([]byte(message))
@@ -154,7 +157,7 @@ func write80(client_to_proxy net.Conn, proxy_to_server net.Conn) {
 
 	readLeng, err := proxy_to_server.Read(buffer)
 	if err != nil {
-		fmt.Println("ERROR9 ", err)
+		fmt.Println("ERROR7 ", err)
 		return
 	}
 	//fmt.Println("WRIIIIIIIIIIIIIIIIIIIIIIT from server:")
@@ -171,7 +174,7 @@ func write80(client_to_proxy net.Conn, proxy_to_server net.Conn) {
 	for {
 		readLeng, err := proxy_to_server.Read(buffer)
 		if err != nil {
-			fmt.Println("ERROR10 ", err)
+			fmt.Println("ERROR8 ", err)
 			return
 		}
 		//fmt.Println("WRIIIIIIIIIIIIIIIIIIIIIIT from server:")
@@ -219,7 +222,7 @@ func write443(client_to_proxy net.Conn, proxy_to_server net.Conn) {
 		if readLeng > 0 {
 			_, err := client_to_proxy.Write(buffer[:readLeng])
 			if err != nil {
-				fmt.Println("ERR4 ", err)
+				fmt.Println("ERR11 ", err)
 				return
 			}
 		}
@@ -233,8 +236,8 @@ func read443(client_to_proxy net.Conn, proxy_to_server net.Conn) {
 	if err != nil {
 		return
 	}
-	//fmt.Println("REEEEEEEEEEEEEEEEEEEEEEED from client:")
-	//fmt.Println(string(buffer[:readLeng]))
+	fmt.Println("REEEEEEEEEEEEEEEEEEEEEEED from client:")
+	fmt.Println(string(buffer[:readLeng]))
 	if readLeng > 0 {
 		_, err := proxy_to_server.Write(buffer[:readLeng])
 		if err != nil {
@@ -250,8 +253,8 @@ func read443(client_to_proxy net.Conn, proxy_to_server net.Conn) {
 		if err != nil {
 			return
 		}
-		//fmt.Println("REEEEEEEEEEEEEEEEEEEEEEED from client:")
-		//fmt.Println(string(buffer[:readLeng]))
+		fmt.Println("REEEEEEEEEEEEEEEEEEEEEEED from client:")
+		fmt.Println(string(buffer[:readLeng]))
 		if readLeng > 0 {
 			_, err := proxy_to_server.Write(buffer[:readLeng])
 			if err != nil {
