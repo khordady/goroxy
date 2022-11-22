fb2a2444f17f76b4f7fe10c8666b874dca27897f
ali <ali_teimoury1@yahoo.com>
2022-11-22 00:41:02 -0600

    idk

---
+++ test/client.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"time"
+)
+
+var bufferSize = 32 * 1024
+
+func main() {
+	client_to_proxy, e := net.Dial("tcp", "185.221.237.166:7070")
+	if e != nil {
+		fmt.Println("ERR2 ", e)
+		return
+	}
+
+	message := []byte("HI this is test")
+
+	writer := bufio.NewWriter(client_to_proxy)
+	_, err := writer.Write(intTobytes(len(message)))
+	if err != nil {
+		fmt.Println("ERR31 ", e)
+		return
+	}
+	_, e = writer.Write(message)
+	if e != nil {
+		fmt.Println("ERR3 ", e)
+		return
+	}
+	err = writer.Flush()
+
+	bufferReader := make([]byte, bufferSize)
+	length, err := readBuffer2(bufferReader, client_to_proxy)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(length)
+}
+
+func readBuffer2(buffer []byte, src net.Conn) (int, error) {
+	size := make([]byte, 4)
+	var total = 0
+	var leng int
+	var errr error
+	fmt.Println("started Reading")
+
+	for leng == 0 {
+		src.SetReadDeadline(time.Now().Add(1 * time.Second))
+		leng, errr = src.Read(size)
+		fmt.Println("LENG is: ", leng)
+		if !os.IsTimeout(errr) && errr != nil {
+			fmt.Println("ERRROR IS: ", errr)
+		}
+	}
+	if leng > 0 {
+		fmt.Println("LENG > 0 ", leng)
+		realSize := bytesToint(size)
+		if realSize <= 0 || realSize > bufferSize {
+			return 0, fmt.Errorf(time.StampMilli, " ERROR OVER SIZE", size)
+		}
+		for total < realSize {
+			length, errrr := src.Read(buffer[total:realSize])
+			fmt.Println("Readed is: ", length)
+			total = total + length
+
+			if errrr != nil {
+				fmt.Println("Total and error is: ", total, errrr)
+				return total, errrr
+			}
+		}
+	}
+	if errr != nil {
+		fmt.Println("Total and error is: ", total, errr)
+		return total, errr
+	}
+	return total, errr
+}
---
+++ test/server.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"time"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":7070")
+	conn, _ := ln.Accept()
+	//err = conn.SetDeadline(time.Time{})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("Connection Received")
+	go handleSocket(conn)
+}
+func handleSocket(client_to_proxy net.Conn) {
+	buffer := make([]byte, 9*1024)
+	reader := bufio.NewReader(client_to_proxy)
+
+	length, err := readBuffer(buffer, reader)
+	fmt.Println(length)
+
+	writer := bufio.NewWriter(client_to_proxy)
+	bytess := []byte("HTTP/1.1 200 Connection Established\r\n\r\n")
+
+	Writelength, err := writer.Write(intTobytes(len(bytess)))
+	if err != nil {
+		fmt.Println(time.StampMilli, " ERROR42 ", err)
+		return
+	}
+	Writelength, err = writer.Write(bytess)
+	if err != nil {
+		fmt.Println(time.StampMilli, " ERROR42 ", err)
+		return
+	}
+	err = writer.Flush()
+	if err != nil {
+		fmt.Println(time.StampMilli, " ERROR42 ", err)
+		return
+	}
+	//client_to_proxy.SetWriteDeadline(time.Now().Add(1 * time.Second))
+
+	if err != nil {
+		fmt.Println(time.StampMilli, " ERROR42 ", err)
+		return
+	}
+	fmt.Println("WROTED 200: ", Writelength)
+}
+
+func readBuffer(buffer []byte, reader *bufio.Reader) (int, error) {
+	size := make([]byte, 4)
+	var total = 0
+
+	fmt.Println("started Reading")
+	leng, errr := reader.Read(size)
+	if leng > 0 {
+		realSize := bytesToint(size)
+		if realSize <= 0 || realSize > bufferSize {
+			return 0, fmt.Errorf(time.StampMilli, " ERROR OVER SIZE")
+		}
+		fmt.Println("Real size is: ", realSize)
+		for total < realSize {
+			length, errrr := reader.Read(buffer[total:realSize])
+			fmt.Println("Readed is: ", length)
+			total = total + length
+
+			if errrr != nil {
+				fmt.Println("Total and error is: ", total, errrr)
+				return total, errrr
+			}
+		}
+	}
+	if errr != nil {
+		fmt.Println("Total and error is: ", total, errr)
+		return total, errr
+	}
+	fmt.Println("Total and error is: ", total, errr)
+	return total, errr
+}
